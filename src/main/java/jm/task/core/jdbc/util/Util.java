3af0cc7e4d6cd9687583a457573a1fbfde307b0b
alexnv32 <99541810+alexnv32@users.noreply.github.com>
2024-07-07 19:31:37 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private final String USERNAME = "alexnv";
+    private final String PASSWORD = "root";
+
+    private Connection connection;
+
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
